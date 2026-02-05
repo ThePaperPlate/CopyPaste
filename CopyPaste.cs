f9ebf3b6680f3001b988f2006cc0c61b7ccd6edd
Mr. Blue <info@mrblue.io>
2026-02-05 19:37:55 +0100

    Bump version

--- CopyPaste.cs
+++ CopyPaste.cs
@@ -40,7 +40,7 @@ using System.Diagnostics;
 
 namespace Oxide.Plugins
 {
-    [Info("Copy Paste", "misticos", "4.2.5")]
+    [Info("Copy Paste", "misticos", "4.2.6")]
     [Description("Copy and paste buildings to save them or move them")]
     public class CopyPaste : CovalencePlugin
     {
